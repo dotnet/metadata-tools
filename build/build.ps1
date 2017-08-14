963ce3b978e0fdc75e32a185e2a6716e1a06b90c
Tomas Matousek <tomat@microsoft.com>
2017-08-14 11:40:32 -0700

    Migrate to RepoToolset v30

--- build/build.ps1
+++ build/build.ps1
@@ -2,25 +2,60 @@
 Param(
   [string] $configuration = "Debug",
   [string] $solution = "",
-  [string] $verbosity = "normal",
+  [string] $verbosity = "minimal",
   [switch] $restore,
   [switch] $build,
+  [switch] $rebuild,
   [switch] $test,
   [switch] $sign,
   [switch] $pack,
-  [switch] $ci
+  [switch] $ci,
+  [switch] $prepareMachine,
+  [switch] $log,
+  [switch] $help,
+  [Parameter(ValueFromRemainingArguments=$true)][String[]]$properties
 )
 
 set-strictmode -version 2.0
 $ErrorActionPreference = "Stop"
 
+function Print-Usage() {
+    Write-Host "Common settings:"
+    Write-Host "  -configuration <value>  Build configuration Debug, Release"
+    Write-Host "  -verbosity <value>      Msbuild verbosity (q[uiet], m[inimal], n[ormal], d[etailed], and diag[nostic])"
+    Write-Host "  -help                   Print help and exit"
+    Write-Host ""
+
+    Write-Host "Actions:"
+    Write-Host "  -restore                Restore dependencies"
+    Write-Host "  -build                  Build solution"
+    Write-Host "  -rebuild                Rebuild solution"
+    Write-Host "  -test                   Run all unit tests in the solution"
+    Write-Host "  -sign                   Sign build outputs"
+    Write-Host "  -pack                   Package build outputs into NuGet packages and Willow components"
+    Write-Host ""
+
+    Write-Host "Advanced settings:"
+    Write-Host "  -solution <value>       Path to solution to build"
+    Write-Host "  -ci                     Set when running on CI server"
+    Write-Host "  -log                    Enable logging (by default on CI)"
+    Write-Host "  -prepareMachine         Prepare machine for CI run"
+    Write-Host ""
+    Write-Host "Command line arguments not listed above are passed thru to msbuild."
+    Write-Host "The above arguments can be shortened as much as to be unambiguous (e.g. -co for configuration, -t for test, etc.)."
+}
+
+if ($help -or (($properties -ne $null) -and ($properties.Contains("/help") -or $properties.Contains("/?")))) {
+  Print-Usage
+  exit 0
+}
 $RepoRoot = Join-Path $PSScriptRoot "..\"
 $DotNetRoot = Join-Path $RepoRoot ".dotnet"
 $DotNetExe = Join-Path $DotNetRoot "dotnet.exe"
 $BuildProj = Join-Path $PSScriptRoot "build.proj"
 $DependenciesProps = Join-Path $PSScriptRoot "Versions.props"
 $ArtifactsDir = Join-Path $RepoRoot "artifacts"
-$LogDir = Join-Path $ArtifactsDir "log"
+$LogDir = Join-Path (Join-Path $ArtifactsDir $configuration) "log"
 $TempDir = Join-Path (Join-Path $ArtifactsDir $configuration) "tmp"
 
 function Create-Directory([string[]] $path) {
@@ -39,7 +74,7 @@ function InstallDotNetCli {
   Create-Directory $DotNetRoot
   $dotnetCliVersion = GetDotNetCliVersion
 
-  $installScript="https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1"
+  $installScript="https://raw.githubusercontent.com/dotnet/cli/release/2.0.0/scripts/obtain/dotnet-install.ps1"
   Invoke-WebRequest $installScript -OutFile "$DotNetRoot\dotnet-install.ps1"
   
   & "$DotNetRoot\dotnet-install.ps1" -Version $dotnetCliVersion -InstallDir $DotNetRoot
@@ -49,27 +84,62 @@ function InstallDotNetCli {
 }
 
 function Build {
-  $summaryLog = Join-Path $LogDir "Build.log"
-  $warningLog = Join-Path $LogDir "Build.wrn"
-  $errorLog = Join-Path $LogDir "Build.err"
+  if ($ci) {
+    Create-Directory($logDir)
+    # Microbuild is on 15.1 which doesn't support binary log
+    if ($env:BUILD_BUILDNUMBER -eq "") {
+      $log = "/bl:" + (Join-Path $LogDir "Build.binlog")
+    } else {
+      $log = "/flp1:Summary;Verbosity=diagnostic;Encoding=UTF-8;LogFile=" + (Join-Path $LogDir "Build.log")
+    }
+  } else {
+    $log = ""
+  }
 
-  Create-Directory($logDir)
-  
-  & $DotNetExe msbuild $BuildProj /p:Configuration=$configuration /p:SolutionPath=$solution /p:Restore=$restore /p:Build=$build /p:Test=$test /p:Sign=$sign /p:Pack=$pack /p:CIBuild=$ci /v:$verbosity /flp1:Summary`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$summaryLog /flp2:WarningsOnly`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$warningLog /flp3:ErrorsOnly`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$errorLog
+  $nodeReuse = !$ci
 
-  if ($lastExitCode -ne 0) {
-    throw "Build failed (exit code '$lastExitCode')."
+  & $DotNetExe msbuild $BuildProj /m /nologo /clp:Summary /nodeReuse:$nodeReuse /warnaserror /v:$verbosity $logCmd /p:Configuration=$configuration /p:SolutionPath=$solution /p:Restore=$restore /p:DeployDeps=$deployDeps /p:Build=$build /p:Rebuild=$rebuild /p:Deploy=$deploy /p:Test=$test /p:IntegrationTest=$integrationTest /p:Sign=$sign /p:Pack=$pack /p:CIBuild=$ci $properties
+}
+
+function Stop-Processes() {
+  Write-Host "Killing running build processes..."
+  Get-Process -Name "dotnet" -ErrorAction SilentlyContinue | Stop-Process
+  Get-Process -Name "vbcscompiler" -ErrorAction SilentlyContinue | Stop-Process
+}
+
+function Clear-NuGetCache() {
+  # clean nuget packages -- necessary to avoid mismatching versions of swix microbuild build plugin and VSSDK on Jenkins
+  $nugetRoot = (Join-Path $env:USERPROFILE ".nuget\packages")
+  if (Test-Path $nugetRoot) {
+    Remove-Item $nugetRoot -Recurse -Force
   }
 }
 
-if ($ci) {
-  Create-Directory $TempDir
-  $env:TEMP = $TempDir
-  $env:TMP = $TempDir
+try {
+  if ($ci) {
+    Create-Directory $TempDir
+    $env:TEMP = $TempDir
+    $env:TMP = $TempDir
+  }
+
+  # Preparation of a CI machine
+  if ($prepareMachine) {
+    Clear-NuGetCache
+  }
+
+  Build
+  exit $lastExitCode
+}
+catch {
+  Write-Host $_
+  Write-Host $_.Exception
+  Write-Host $_.ScriptStackTrace
+  exit 1
+}
+finally {
+  Pop-Location
+  if ($ci -and $prepareMachine) {
+    Stop-Processes
+  }
 }
 
-if ($restore) {
-  InstallDotNetCli
-}
-
-Build
